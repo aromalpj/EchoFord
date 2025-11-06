6ff6f643011159524f0197ebd188ea7c220fa15b
aromalpj <rahul.raghukumar@trenser.com>
2025-11-06 11:22:26 +0530

    Modified dht11km.mk and i2c_client_driver.mk

--- externalapp/src/driver/i2cappClient/i2c_client_driver.mk
+++ externalapp/src/driver/i2cappClient/i2c_client_driver.mk
@@ -1,4 +1,4 @@
-I2C_CLIENT_SITE = $(BR2_EXTERNAL_ECHOFORD_EXTERNAL_APP_SAMPLE_PATH)/src/driver/i2cappClient
+I2C_CLIENT_SITE = $(BR2_EXTERNAL_ECHOFORD_EXTERNAL_APP_PATH)/src/driver/i2cappClient
 I2C_CLIENT_SITE_METHOD = local
 
 $(eval $(kernel-module))
